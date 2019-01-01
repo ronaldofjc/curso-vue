6bcd320cb738f14f488a4891bdadb04e7a71c14e
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-01-01 20:08:43 -0200

    Area do Jogo - Botoes - Resultado

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,10 +1,14 @@
 new Vue({
   el: '#app',
   data: {
-
+    running: false,
+    playerLife: 0,
+    monsterLife: 100,
   },
   computed: {
-
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0
+    }
   },
   methods: {
 
