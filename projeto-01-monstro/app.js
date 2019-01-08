8bc8a0f537798d9c3ddac88fd115bbcb9db363a4
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-01-08 09:18:34 -0200

    Atacar

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -2,7 +2,7 @@ new Vue({
   el: '#app',
   data: {
     running: false,
-    playerLife: 0,
+    playerLife: 100,
     monsterLife: 100,
   },
   computed: {
@@ -15,9 +15,24 @@ new Vue({
       this.running = true
       this.playerLife = 100
       this.monsterLife = 100
+    },
+    attack(special) {
+      this.hurt('playerLife', 7, 12, false)
+      this.hurt('monsterLife', 5, 10, special)
+    },
+    hurt(prop, min, max, special) {
+      const plus = special ? 5 : 0
+      const hurt = this.getRandom(min + plus, max + plus)
+      this[prop] = Math.max(this[prop] - hurt, 0)
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min
+      return Math.round(value)
     }
   },
   watch: {
-
+    hasResult(value) {
+      if (value) this.running = false
+    }
   }
 })
