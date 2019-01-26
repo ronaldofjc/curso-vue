5674a9f9b9949cbebf9d92f52d629e4ae63a3a23
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-01-26 18:28:01 -0200

    Vue CLi

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <span>{{ contador }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    };
+  }
+};
+</script>
+
+<style>
+</style>
