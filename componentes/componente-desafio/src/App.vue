e611b2996899a9b87292e0be56c6233f6153c54b
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-01-28 10:41:55 -0200

    Organizando App em Componentes

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <app-navigation/>
+
+    <app-toolbar/>
+
+    <app-content/>
+
+    <app-footer/>
+  </v-app>
+</template>
+
+<script>
+import Navigation from "./Navigation.vue";
+import Toolbar from "./Toolbar.vue";
+import Content from "./Content.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: {
+    "app-navigation": Navigation,
+    "app-toolbar": Toolbar,
+    "app-content": Content,
+    "app-footer": Footer
+  }
+};
+</script>
---
+++ componentes/componente-desafio/src/Content.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-content>
+      <v-carousel>
+        <v-carousel-item v-for="(item,i) in fotos" :key="i" :src="item.src"></v-carousel-item>
+      </v-carousel>
+    </v-content>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fotos: [
+        { src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" },
+        { src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg" },
+        { src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg" },
+        { src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
