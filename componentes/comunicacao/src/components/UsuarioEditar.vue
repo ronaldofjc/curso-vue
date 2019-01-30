34c628e106d6de164b91cae08f47463893f28a82
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-01-30 09:18:33 -0200

    Comunicacao direta entre componentes com props

--- componentes/comunicacao/src/components/UsuarioEditar.vue
+++ componentes/comunicacao/src/components/UsuarioEditar.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class="componente">
-        <h2>Alterar os Dados de Usuário</h2>
-        <p>Edite as informações</p>
-    </div>
+  <div class="componente">
+    <h2>Alterar os Dados de Usuário</h2>
+    <p>Edite as informações</p>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #98b99a;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #98b99a;
+  color: #fff;
+}
 </style>
