cf16d66f8fa58a69f7e73b45834ff756e8c8a2d2
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-02-01 09:34:45 -0200

    Projeto 02 - Componente Lista de Tarefas

---
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <TaskGrid :tasks="tasks"/>
+  </div>
+</template>
+
+<script>
+import TaskGrid from "./components/TaskGrid.vue";
+
+export default {
+  components: { TaskGrid },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar Louça", pending: false },
+        { name: "Comprar blusa", pending: true }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
+
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
+</style>
