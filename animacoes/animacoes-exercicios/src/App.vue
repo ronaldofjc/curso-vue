f01ea9d8a1506dcbb43fa7d0ae0db0962ac10a01
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-02-10 11:37:39 -0200

    Definindo as classes css para transicao

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -3,7 +3,7 @@
     <h1>Animações</h1>
     <hr>
     <b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
-    <transition>
+    <transition name="fade">
       <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
     </transition>
   </div>
@@ -29,5 +29,23 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.fade-enter {
+}
+
+.fade-enter-active {
+}
+
+.fade-enter-to {
+}
+
+.fade-leave {
+}
+
+.fade-leave-active {
+}
+
+.fade-leave-to {
 }
 </style>
