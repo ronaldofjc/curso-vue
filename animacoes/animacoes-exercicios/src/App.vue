a3b9c96f432d2cbb8656a7cef0458a18f4172054
Ronaldo Feitosa <rfeitosa@lnxcit019409.cit>
2019-02-16 14:44:19 -0200

    Entendendo animacao em Javascript

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -21,6 +21,20 @@
       <b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
       <b-alert variant="warning" show v-else key="warn">{{ msg }}</b-alert>
     </transition>
+    <hr>
+    <button @click="exibir2 = !exibir2">Mostrar</button>
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leaveCancelled="leaveCancelled"
+    >
+      <div v-if="exibir2" class="caixa"></div>
+    </transition>
   </div>
 </template>
 
@@ -30,8 +44,37 @@ export default {
     return {
       msg: "Uma mensagem de informação para o usuário!",
       exibir: false,
+      exibir2: true,
       tipoAnimacao: "fade"
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter");
+    },
+    enter(el, done) {
+      console.log("enter");
+      done();
+    },
+    afterEnter(el) {
+      console.log("afterEnter");
+    },
+    enterCancelled() {
+      console.log("enterCancelled");
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave");
+    },
+    leave(el, done) {
+      console.log("leave");
+      done();
+    },
+    afterLeave(el) {
+      console.log("afterLeave");
+    },
+    leaveCancelled() {
+      console.log("leaveCancelled");
+    }
   }
 };
 </script>
@@ -45,6 +88,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.caixa {
+  height: 100px;
+  width: 300px;
+  margin: 30px auto;
+  background-color: lightgreen;
 }
 
 .fade-enter,
