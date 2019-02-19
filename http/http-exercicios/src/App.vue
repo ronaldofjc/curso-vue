d8662d804d5b74f5c83cbc5751631b2db12efbe0
Ronaldo <ronaldofjc@hotmail.com>
2019-02-19 16:42:18 -0300

    Criando Formulario

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,17 +1,44 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome">
+				<b-form-input 
+					type="text" size="lg" v-model="usuario.nome" placeholder="Informe seu nome">
+				</b-form-input>
+			</b-form-group>
+			<b-form-group label="E-mail">
+				<b-form-input 
+					type="email" size="lg" v-model="usuario.email" placeholder="Informe seu email">
+				</b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button variant="primary" size="lg" @click="salvar">Salvar</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
-	created() {
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		salvar() {
+			console.log(this.usuario)
+		}
+	}
+	/*created() {
 		this.$http.post('usuarios.json', {
 			nome: 'Maria',
 			email: 'maria_maria@gmail.com'
 		}).then(res => console.log(res))
-	}
+	}*/
 }
 </script>
 	
