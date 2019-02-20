928dd38a247fd28fa9b812d35ab2964d3d0c822f
Ronaldo <ronaldofjc@hotmail.com>
2019-02-20 10:41:14 -0300

    Interceptando requisicoes

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://curso-vue-1c117.firebaseio.com/'
+//axios.defaults.baseURL = 'https://curso-vue-1c117.firebaseio.com/'
 
 Vue.use({
 	install(Vue) {
-		Vue.prototype.$http = axios
+		Vue.prototype.$http = axios.create({
+			baseURL: 'https://curso-vue-1c117.firebaseio.com/'
+		})
+
+		Vue.prototype.$http.interceptors.request.use(config => {
+			//console.log(config.method)
+			return config
+		})
 	}
 })
