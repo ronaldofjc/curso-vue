819bc44f9e81bcbd1502dd7821c70ee288edacc9
Ronaldo <ronaldofjc@hotmail.com>
2019-02-25 19:17:29 -0300

    Lendo e usando parametros de rotas

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,12 +2,18 @@
   <div class="usuario">
     <h2>Usuários</h2>
     <hr>
+    <p><strong>ID: {{ id }}</strong></p>
     <button @click="irParaInicio" sucesso>Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  },
   methods: {
     irParaInicio() {
       this.$router.push('/')
