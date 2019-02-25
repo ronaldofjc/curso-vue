4fb71254dd5a29574cc04e2b214dc1844d330a2f
Ronaldo <ronaldofjc@hotmail.com>
2019-02-25 19:52:05 -0300

    Configurando rotas filhas

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,24 +2,13 @@
   <div class="usuario">
     <h2>Usuários</h2>
     <hr>
-    <p><strong>ID: {{ id }}</strong></p>
+    <router-view />
     <button @click="irParaInicio" sucesso>Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['id'],
-  /*data() {
-    return {
-      id: this.$route.params.id
-    }
-  },
-  watch: {
-    $route(to, from) {
-      this.id = to.params.id
-    }
-  },*/
   methods: {
     irParaInicio() {
       this.$router.push('/')
---
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="usuario-detalhe">
+    <h3>Usuário Detalhe</h3>
+    <hr>
+    <p><strong>ID: {{ id }}</strong></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  /*data() {
+    return {
+      id: this.$route.params.id
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id
+    }
+  },*/
+}
+</script>
+
+<style>
+
+</style>
