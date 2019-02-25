5af5236499749597a9dff0ab981cca5581da34b4
Ronaldo <ronaldofjc@hotmail.com>
2019-02-25 19:11:44 -0300

    Navegacao Imperativa

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="usuario">
     <h2>Usuários</h2>
+    <hr>
+    <button @click="irParaInicio" sucesso>Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    irParaInicio() {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
