5cb815b12ae9df20494bff232d7d1cd2b7424617
Ronaldo <ronaldofjc@hotmail.com>
2019-02-27 10:38:56 -0300

    Carregando rotas tardiamente

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from './components/Inicio'
-import Usuario from './components/usuario/Usuario'
-import UsuarioLista from './components/usuario/UsuarioLista'
-import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
-import UsuarioEditar from './components/usuario/UsuarioEditar'
+//import Usuario from './components/usuario/Usuario'
+//import UsuarioLista from './components/usuario/UsuarioLista'
+//import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
+//import UsuarioEditar from './components/usuario/UsuarioEditar'
 
 Vue.use(Router)
+
+const Usuario = () => import(/* webpackChunkName: "usuario" */'./components/usuario/Usuario')
+const UsuarioLista = () => import(/* webpackChunkName: "usuario" */'./components/usuario/UsuarioLista')
+const UsuarioDetalhe = () => import(/* webpackChunkName: "usuario" */'./components/usuario/UsuarioDetalhe')
+const UsuarioEditar = () => import(/* webpackChunkName: "usuario" */'./components/usuario/UsuarioEditar')
 
 export default new Router({
   mode:'history',
