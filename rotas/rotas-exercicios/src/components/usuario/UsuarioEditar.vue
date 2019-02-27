10958c7a89fbee1568cfb072fa2a82d3d2051317
Ronaldo <ronaldofjc@hotmail.com>
2019-02-27 10:19:05 -0300

    Usando o evento BeforeRouteLeave

--- rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -4,6 +4,8 @@
     <p><strong>ID: {{ id }}</strong></p>
     <p><strong>Completo: </strong>{{ $route.query.completo ? 'Sim' : 'Não' }}</p>
     <p><strong>Lingua: </strong>{{ $route.query.lingua }}</p>
+    <hr>
+    <button primario @click="confirmou = true">Confirmar</button>
     <div id="rodape">
       <h3>Curso Vue</h3>
     </div>
@@ -12,7 +14,23 @@
 
 <script>
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      confirmou: false
+    }
+  },
+  beforeRouteLeave (to, from, next) {
+    if(this.confirmou) {
+      next()
+    } else {
+      if(confirm('Tem certeza?')) {
+        next()
+      } else {
+        next(false)
+      }
+    }
+  }
 }
 </script>
 
