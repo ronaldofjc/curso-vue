bcfe42ad135cb284f6f3ee633efbd6e40f84c485
Ronaldo <ronaldofjc@hotmail.com>
2019-03-02 16:15:47 -0300

    Componentes iniciais e rotas

--- projeto-03-acoes/stock-trader/src/App.vue
+++ projeto-03-acoes/stock-trader/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-	<h1>Stock Trader</h1>
+  <v-app>
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
 
